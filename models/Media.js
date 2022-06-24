f8404b7c7c751caee0677642b2f166a5daa18ad1
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-06-24 23:39:42 +0700

    create model for media

---
+++ models/Media.js
@@ -0,0 +1,30 @@
+module.exports = (Sequelize, DataTypes) => {
+  const Media = sequelize.define(
+    'Media',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'media',
+    }
+  );
+
+  return Media;
+};
