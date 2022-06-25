57cee7b0f0f219679004f8ffe6f6ea9186c1de68
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-06-25 23:31:57 +0700

    Post Image

--- routes/media.js
+++ routes/media.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 
+const { Media } = require('../models');
+
 router.post('/', (req, res) => {
   const image = req.body.image;
 
@@ -10,7 +12,22 @@ router.post('/', (req, res) => {
     return res.status(400).json({ status: 'error', message: 'Invalid base64' });
   }
 
-  return res.send('ok');
+  base64Img.img(image, './public/images', Date.now(), async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({ status: 'error', message: err.message });
+    }
+
+    const fileName = filepath.split('\\').pop().split('/').pop();
+    const media = await Media.create({ image: `image/${fileName}` });
+
+    return res.json({
+      status: 'success',
+      data: {
+        id: media.id,
+        image: `${req.get('host')}/images/${fileName}`,
+      },
+    });
+  });
 });
 
 module.exports = router;
